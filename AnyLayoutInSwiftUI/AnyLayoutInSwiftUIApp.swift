a203fd218b98124b970fbe02005e43464789b6e8
Ramill <ramill_ibragimov@icloud.com>
2022-10-05 13:00:18 +0300

    Initial Commit

---
+++ AnyLayoutInSwiftUI/AnyLayoutInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnyLayoutInSwiftUIApp.swift
+//  AnyLayoutInSwiftUI
+//
+//  Created by Ramill Ibragimov on 05.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnyLayoutInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
